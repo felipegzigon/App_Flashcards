bd80c14401d1858d9ae75845d6a9bf33de3b1fcb
felipe <felipe.zigon@fatec.sp.gov.br>
2026-01-30 00:05:37 -0300

    init2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PaginaInicialComponent } from './pages/pagina-inicial/pagina-inicial.component';
+import { CardsComponent } from './pages/cards/cards.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: 'pagina-inicial', component: PaginaInicialComponent },
+  { path: 'cards', component: CardsComponent },
+  { path: '', redirectTo: '/pagina-inicial', pathMatch: 'full' },
+  { path: '**', redirectTo: '/pagina-inicial' }
+];
